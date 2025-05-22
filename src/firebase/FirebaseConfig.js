b17179ba0e2c873df76d262ceebef79c2b13008e
Sinan Turan <turansinan2004@gmail.com>
2025-05-22 12:41:32 +0300

    identity check with firebase

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDgS4vjp7l5w3kJZymI0rLd7eAkXRv68qg",
-    authDomain: "tge-app-d6c00.firebaseapp.com",
-    projectId: "tge-app-d6c00",
-    storageBucket: "tge-app-d6c00.firebasestorage.app",
-    messagingSenderId: "375138509496",
-    appId: "1:375138509496:web:f2ed231e25cc513398ca4d"
+    apiKey: "abc",
+    authDomain: "abc",
+    projectId: "abc",
+    storageBucket: "abc",
+    messagingSenderId: "abc",
+    appId: "abc"
 }
 
 export const app = initializeApp(firebaseConfig);
